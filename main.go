c0ad2f8a5ab65ee024fc161e3b0b3ce4d3a6f97b
snowmerak <snowmerak@outlook.com>
2025-04-19 02:40:58 +0900

    refactor main function to integrate MeiliSearch for library suggestions and enhance error handling

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os/signal"
 	"syscall"
 
+	"github.com/snowmerak/hasu/internal/document"
+	"github.com/snowmerak/hasu/internal/meilisearch"
 	"github.com/snowmerak/hasu/internal/ollama"
 	"github.com/snowmerak/hasu/internal/prompts"
 )
@@ -13,6 +16,12 @@ import (
 func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer cancel()
+	_ = ctx
+
+	mc := meilisearch.New(meilisearch.NewConfig().WithApiKey(meilisearch.MasterKey))
+	if err := meilisearch.Insert[document.Library](mc, document.IndexNameLibraries, document.GetDefaultLibraries()...); err != nil {
+		panic(err)
+	}
 
 	cli, err := ollama.New(ollama.Config{
 		Model: ollama.ModelGemma3p12B,
@@ -35,5 +44,23 @@ func main() {
 
 	// Extract suggested technologies
 	suggestedTechnologies := prompts.ExtractSuggestedTechnologies(resp)
-	fmt.Printf("Suggested Technologies: %+v", suggestedTechnologies)
+	for _, tech := range suggestedTechnologies {
+		sr, err := meilisearch.Search[document.Library](mc, document.IndexNameLibraries, tech, meilisearch.SearchOption{
+			Limit:  5,
+			Offset: 0,
+		})
+		if err != nil {
+			log.Printf("Error searching technologies: %v", err)
+			continue
+		}
+
+		if len(sr.Hits) == 0 {
+			log.Printf("No libraries found for technology: %s", tech)
+			continue
+		}
+
+		for _, lib := range sr.Hits {
+			fmt.Printf("Library ID: %s, Name: %s, URL: %s, Tags: %v, Description: %s\n", lib.ID, lib.Name, lib.URL, lib.Tags, lib.Description)
+		}
+	}
 }
