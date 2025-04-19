76de1f48516a2f32332aafdf911ecc38c12e65b0
snowmerak <snowmerak@outlook.com>
2025-04-19 12:22:24 +0900

    remove hardcoded libraryList from document.go to improve flexibility and maintainability

--- internal/document/document.go
+++ internal/document/document.go
@@ -13,13 +13,3 @@ type Library struct {
 func GetDefaultLibraries() []Library {
 	return libraryList
 }
-
-var libraryList = []Library{
-	{
-		ID:          "1",
-		Name:        "rs/zerolog",
-		URL:         "https://github.com/rs/zerolog",
-		Tags:        []string{"logger", "golang", "zero allocation", "performance"},
-		Description: "rs/zerolog is a fast and simple logger for Go. It is designed to be zero allocation and high performance, making it ideal for high-throughput applications.",
-	},
-}
