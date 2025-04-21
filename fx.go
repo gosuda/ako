50e95bc07a0408d5555c1bed3fe735f9781bce72
snowmerak <snowmerak@outlook.com>
2025-04-21 23:20:58 +0900

    Add function to create FX file with template in fx.go

--- fx.go
+++ fx.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
 const (
 	fxFileName          = "init.go"
 	fxDependencyPackage = "go.uber.org/fx"
@@ -11,6 +18,48 @@ func getFxDependency() error {
 	}
 
 	if err := tidyGoModule(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+const fxFileTemplate = `package %s
+
+import (
+	"context"
+
+	"go.uber.org/fx"
+)
+
+type %s struct {
+}
+
+func New%s(ctx context.Context, lc fx.Lifecycle) *%s {
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			// Initialize the client here if needed
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			// Clean up resources if needed
+			return nil
+		},
+	})
+	return &%s{}
+}`
+
+func createFxFile(path string, name string) error {
+	packageName := filepath.Base(path)
+	file, err := os.Create(filepath.Join(path, fxFileName))
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	name = strings.ToUpper(name[:1]) + name[1:]
+
+	if _, err := fmt.Fprintf(file, fxFileTemplate, packageName, name, name, name, name); err != nil {
 		return err
 	}
 
