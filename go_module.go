24ef72a6f0e39cd49536521d2c2439bd7028d281
snowmerak <snowmerak@outlook.com>
2025-04-21 22:06:58 +0900

    Add go_module.go for Go module initialization and management functions

---
+++ go_module.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+)
+
+func initGoModule(moduleName string) error {
+	cmd := exec.Command("go", "mod", "init", moduleName)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getGoModule(item string) error {
+	cmd := exec.Command("go", "get", item)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getGoModuleAsTool(item string) error {
+	cmd := exec.Command("go", "get", "-tool", item)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func tidyGoModule() error {
+	cmd := exec.Command("go", "mod", "tidy")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
