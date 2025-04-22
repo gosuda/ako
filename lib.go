bc1eae0258ea6295e713c409e3587931c1242534
snowmerak <snowmerak@outlook.com>
2025-04-22 22:22:53 +0900

    Add library management functions in lib.go for enhanced library creation and selection

--- lib.go
+++ lib.go
@@ -1,5 +1,94 @@
 package main
 
-const (
-	library
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/AlecAivazis/survey/v2"
 )
+
+const (
+	libraryAdapter = "adapter"
+	libRepository  = "repository"
+	libDomain      = "domain"
+)
+
+func makeLibraryPath(base string, category string) string {
+	return filepath.Join("lib", base, category)
+}
+
+func selectLibraryBase() (string, error) {
+	candidates := []string{
+		libraryAdapter + ": Defines interfaces abstracting communication with external systems (APIs, message queues, etc.).",
+		libRepository + ": Defines interfaces for accessing the data persistence layer (DB, cache, etc.).",
+		libDomain + ": Defines core business logic, rules, domain models, and domain service interfaces.",
+	}
+	var base string
+	if err := survey.AskOne(&survey.Select{
+		Message: "Select the library category:",
+		Options: candidates,
+	}, &base, survey.WithValidator(survey.Required)); err != nil {
+		return "", err
+	}
+
+	sp := strings.Split(base, ":")
+	base = strings.TrimSpace(sp[0])
+
+	if base != libraryAdapter && base != libRepository && base != libDomain {
+		return "", fmt.Errorf("invalid library base: %s", base)
+	}
+
+	return base, nil
+}
+
+func inputLibraryPackage() (string, error) {
+	var packageName string
+	if err := survey.AskOne(&survey.Input{
+		Message: "Enter the library package name:",
+	}, &packageName, survey.WithValidator(survey.Required)); err != nil {
+		return "", err
+	}
+
+	packageName = strings.TrimSpace(packageName)
+	if packageName == "" {
+		return "", fmt.Errorf("invalid library packageName: %s", packageName)
+	}
+
+	return packageName, nil
+}
+
+func inputLibraryName() (string, error) {
+	var name string
+	if err := survey.AskOne(&survey.Input{
+		Message: "Enter the library name:",
+	}, &name, survey.WithValidator(survey.Required)); err != nil {
+		return "", err
+	}
+
+	name = strings.TrimSpace(name)
+	if name == "" {
+		return "", fmt.Errorf("invalid library name: %s", name)
+	}
+
+	return name, nil
+}
+
+func createLibraryFile(path string, name string) error {
+	if err := os.MkdirAll(path, os.ModePerm); err != nil {
+		return err
+	}
+
+	packageName := filepath.Base(path)
+
+	fileName := filepath.Join(path, fmt.Sprintf(fxFileName, name))
+	if err := writeTemplate2File(fileName, fxInterfaceFileTemplate, map[string]any{
+		"package_name": packageName,
+		"client_name":  name,
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
