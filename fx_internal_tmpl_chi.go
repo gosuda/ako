6320bb58c3b8056fa3aa52e2e83cb306f84693b6
snowmerak <snowmerak@outlook.com>
2025-05-02 01:52:54 +0900

    feat(cli): add Chi server template and file generation support

---
+++ fx_internal_tmpl_chi.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func init() {
+	internalControllerTemplateList["chi (http, https)"] = createFxChiFile
+}
+
+const (
+	chiDependency     = `github.com/go-chi/chi/v5`
+	chiServerTemplate = `package {{.package_name}}
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"go.uber.org/fx"
+)
+
+// Register is the fx.Provide function for the client.
+// It registers the client as a dependency in the fx application.
+// You can append interfaces into the fx.As() function to register multiple interfaces.
+var Register = fx.Provide(New, ConfigRegister())
+
+func ConfigRegister() func() *Config {
+	return func() *Config {
+		return &Config{
+			Addr: os.Getenv("FIBER_{{.server_name}}_ADDR"),
+		}
+	}
+}
+
+type Param struct {
+	fx.In
+	Cfg *Config
+}
+
+type {{.server_name}} struct {
+	mux *chi.Mux
+	svr *http.Server
+}
+
+type Config struct {
+	Addr string
+}
+
+func New(ctx context.Context, lc fx.Lifecycle, param Param) *{{.server_name}} {
+	mux := chi.NewRouter()
+	mux.Use(middleware.Heartbeat("/ping"))
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.RequestID)
+	mux.Use(middleware.RealIP)
+	mux.Use(middleware.Timeout(60 * time.Second))
+
+	svr := &{{.server_name}}{
+		mux: mux,
+		svr: &http.Server{
+			Addr:    param.Cfg.Addr,
+			Handler: mux,
+		},
+	}
+
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			go func() {
+				if err := svr.svr.ListenAndServe(); err != nil {
+					log.Printf("Error starting server: %v\n", err)
+				}
+			}()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return svr.svr.Shutdown(ctx)
+		},
+	})
+
+	return svr
+}`
+)
+
+func createFxChiFile(path string, name string) error {
+	if err := os.MkdirAll(path, os.ModePerm); err != nil {
+		return err
+	}
+
+	name = strings.ToUpper(name[:1]) + name[1:]
+
+	packageName := filepath.Base(path)
+	if err := writeTemplate2File(filepath.Join(path, fmt.Sprintf(fxFileName, name)), chiServerTemplate, map[string]any{
+		"package_name": packageName,
+		"server_name":  name,
+	}); err != nil {
+		return err
+	}
+
+	if err := getGoModule(chiDependency); err != nil {
+		return fmt.Errorf("getGoModule: %w", err)
+	}
+
+	return nil
+}
