85be1dd075ca9a4a87eb3b1face91903d7f174e0
snowmerak <snowmerak@outlook.com>
2025-05-12 02:39:08 +0900

    feat(ai): enhance commit message generation with support for Vertex AI and Gemini clients

--- generator/ai/llm_config.go
+++ generator/ai/llm_config.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 
+	"google.golang.org/genai"
 	"gopkg.in/yaml.v3"
 )
 
@@ -20,10 +21,16 @@ type Config struct {
 	} `yaml:"ollama"`
 	Gemini struct {
 		Enable bool   `yaml:"enable"`
-		Host   string `yaml:"host"`
 		Model  string `yaml:"model"`
 		APIKey string `yaml:"api_key"`
 	} `yaml:"gemini"`
+	Vertex struct {
+		Enable   bool   `yaml:"enable"`
+		Model    string `yaml:"model"`
+		APIKey   string `yaml:"api_key"`
+		Location string `yaml:"location"`
+		Project  string `yaml:"project"`
+	}
 	Anthropic struct {
 		Enable bool   `yaml:"enable"`
 		Host   string `yaml:"host"`
@@ -107,8 +114,21 @@ func GenerateCommitMessage(ctx context.Context, gitDiff string) (<-chan string, 
 		return nil, fmt.Errorf("config file not exists")
 	}
 
-	if GlobalConfig.Ollama.Enable {
+	switch {
+	case GlobalConfig.Ollama.Enable:
 		client, err := NewOllamaClient(GlobalConfig.Ollama.Host, GlobalConfig.Ollama.Model)
+		if err != nil {
+			return nil, err
+		}
+		return client.GenerateCommitMessage(ctx, gitDiff)
+	case GlobalConfig.Gemini.Enable:
+		client, err := NewGeminiClient(genai.BackendGeminiAPI, GlobalConfig.Gemini.APIKey, GlobalConfig.Gemini.Model, "", "")
+		if err != nil {
+			return nil, err
+		}
+		return client.GenerateCommitMessage(ctx, gitDiff)
+	case GlobalConfig.Vertex.Enable:
+		client, err := NewGeminiClient(genai.BackendVertexAI, GlobalConfig.Vertex.APIKey, GlobalConfig.Vertex.Model, GlobalConfig.Vertex.Location, GlobalConfig.Vertex.Project)
 		if err != nil {
 			return nil, err
 		}
